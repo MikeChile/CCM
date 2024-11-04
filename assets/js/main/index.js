70064f21897fcd7c8c25b67eddae7d11fc200d0b
Miguel Andrades <mxzthemaster@gmail.com>
2024-11-04 17:26:41 -0300

    agregado pagina de admision 2025, pagina de noticias, creación de API de noticias, algunas correcciones

---
+++ assets/js/main/index.js
@@ -0,0 +1,7 @@
+// assets/js/main/index.js
+import { cargarFooter } from '../components/footer';
+
+// Llamar a la función para cargar el footer al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    cargarFooter();
+});
